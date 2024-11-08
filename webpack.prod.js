16b91e3411aeb4ef11655d2caac5eeb16e9b4127
Natallia Pazniak <natalyua_nata_@mail.ru>
2024-11-08 16:57:05 +0300

    fix: paths to pictures

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    publicPath: "./",
+    publicPath: "/",
   },
   optimization: {
     minimize: true,
